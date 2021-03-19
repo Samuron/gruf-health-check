9518083ac7cd4d6bb52ba1c95bfdeb92f77b35cf
Ievgen Makukh <samuronchik@gmail.com>
2021-03-19 09:31:21 +0200

    chore: reduce number of modules

--- app/rpc/health_controller.rb
+++ app/rpc/health_controller.rb
@@ -1,17 +1,11 @@
 require 'grpc/health/v1/health_services_pb'
 
-module Grpc
-  module Health
-    module V1
-      module Health
-        class HealthController < ::Gruf::Controllers::Base
-          bind Grpc::Health::V1::Health::Service
+module Health
+  class HealthController < ::Gruf::Controllers::Base
+    bind Grpc::Health::V1::Health::Service
 
-          def check
-            Grpc::Health::V1::HealthCheckResponse.new(status: Grpc::Health::V1::HealthCheckResponse::ServingStatus::SERVING)
-          end
-        end
-      end
+    def check
+      Grpc::Health::V1::HealthCheckResponse.new(status: Grpc::Health::V1::HealthCheckResponse::ServingStatus::SERVING)
     end
   end
 end
